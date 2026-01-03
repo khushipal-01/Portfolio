c0755584e83f04c02410fce9d59348644df9ad66
khushi pal <165002584+khushipal-01@users.noreply.github.com>
2026-01-03 16:14:35 +0530

    Add typing effect and update project details

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const projects = [
     title: "Suspicious Link Scanner",
     desc: "A cybersecurity tool to detect malicious links.",
     image: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?auto=format&fit=crop&w=400&q=80",
-    live: "#",
+    live: "https://khushipal-01.github.io/Portfolio/",
     github: "#"
   },
   {
@@ -95,4 +95,5 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll('.section').forEach(section => {
   observer.observe(section);
 });
+
 
